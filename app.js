0b8361b0ce2c8171490d01d451121a87c18d336b
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-17 14:37:26 +0300

    add ionic list

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const inputReason = document.querySelector('#input-reason');
 const inputAmount = document.querySelector('#input-amount');
 const cancelButton = document.querySelector('#btn-cancel');
 const addButton = document.querySelector('#btn-add');
+const expensesList = document.querySelector('#expenses-list');
+const totalExpenses = document.querySelector('#total-expenses');
+let total = 0;
+
 
 // add button
 addButton.addEventListener('click', ($event) => {
@@ -12,6 +16,29 @@ addButton.addEventListener('click', ($event) => {
         return;
     }
 
-    console.log(reason,amount)
+    // add the amount
+    total = total + (+amount);
+    // create the new item
+    const newItem = document.createElement('ion-item');
+    newItem.textContent  = reason + ': $' + amount;
+
+    // append the new item
+    expensesList.appendChild(newItem);
+
+    // add the total
+    totalExpenses.textContent = total.toString();
+
+    // clear
+    clear();
+
+});
+
+// clear function
+function clear(){
+    inputReason.value = '';
+    inputAmount.value = '';
+}
+cancelButton.addEventListener('click',()=>{
+    clear();
 })
 
